41fc6f3eedb8a56d18461d8063fbda1ec03bb439
Rodolfo Castellanos <rodolfojcj@yahoo.com>
2014-05-24 17:01:01 +0000

    Little fix in the license text

--- recipes/install_drush.rb
+++ recipes/install_drush.rb
@@ -4,8 +4,6 @@
 #
 # Copyright 2014, OpenSinergia
 #
-# All rights reserved - Do Not Redistribute
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
