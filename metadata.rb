60f8b04d8e57daa3eb5f6fbe8cbe326a17caf520
Rodolfo Castellanos <rodolfojcj@yahoo.com>
2014-12-05 22:32:03 -0430

    Fix in virtual host configuration for Apache 2.4 (or older versions too)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rodolfojcj at yahoo.com'
 license          'Apache v2.0'
 description      'Installs a web site based on Drupal CMS using Drush as the workhorse for (almost) everything'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.5'
+version          '0.1.6'
 
 depends "apache2"
 depends "database"
